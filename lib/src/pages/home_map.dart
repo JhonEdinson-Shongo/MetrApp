2a7bff6f6c5b6de96128da3f60a1d425a4aee7ab
juanca9918 <49576631+juanca9918@users.noreply.github.com>
2020-04-22 15:34:42 -0500

    Mapa ERROR DE TAMAÑO

--- lib/src/pages/home_map.dart
+++ lib/src/pages/home_map.dart
@@ -1,44 +1,115 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+class Mapa extends StatefulWidget {
+  @override
+  createState() => _Mapa();
+}
 
-
-class Mapa extends StatelessWidget {
+class _Mapa extends State<Mapa> {
   Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(7.1087037, -73.1172089),
-    zoom: 14.4746,
+  static final CameraPosition _position1 = CameraPosition(
+    bearing: 192.833,
+    target: LatLng(45.521563, -122.677433),
+    tilt: 59.440,
+    zoom: 11.0,
   );
 
+  Future<void> _goToPosition1() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_position1));
+  }
 
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("MetrApp"),
-        backgroundColor: Color.fromRGBO(39, 99, 52, 1),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: 500.0,
-              child: GoogleMap(
-                mapType: MapType.hybrid,
-                initialCameraPosition: _kGooglePlex,
-                onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
-                },
-              ),
-            ),
-          ],
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'This is a Title',
+          snippet: 'This is a snippet',
         ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  Widget button(Function function, IconData icon) {
+    return FloatingActionButton(
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        icon,
+        size: 36.0,
       ),
-      
     );
   }
-          
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('MetrAPP'),
+              backgroundColor: Colors.green,
+            ),
+            body: Stack(
+              children: <Widget>[
+                GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: _center,
+                    zoom: 11.0,
+                ),
+                mapType: _currentMapType,
+                markers: _markers,
+                onCameraMove: _onCameraMove,
+                ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: <Widget>[
+                            button(_onMapTypeButtonPressed, Icons.map),
+                            SizedBox(
+                              height: 16.0,
+                            ),
+                            button(
+                                _onAddMarkerButtonPressed, Icons.add_location),
+                            SizedBox(
+                              height: 16.0,
+                            ),
+                            button(_goToPosition1, Icons.location_searching),
+                          ],
+                        )),
+                  ),
+                ],
+            ),
+          ),
+    );
+  }
 }
